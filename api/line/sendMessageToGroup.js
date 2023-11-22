36f2573e43b32242ad4241beae8ef69c59b0c25a
yukendev <yuken0906dev@gmail.com>
2023-11-22 16:34:31 +0900

    feat: method for sending message to group

---
+++ api/line/sendMessageToGroup.js
@@ -0,0 +1,28 @@
+import https from "https";
+
+export const sendMessageToGroup = (message) => {
+  const myId = process.env.YUKEN_LINE_ID;
+  //   const groupId
+  const token = process.env.ACCESS_TOKEN;
+
+  const data = JSON.stringify({
+    to: myId,
+    messages: [
+      {
+        type: "text",
+        text: message,
+      },
+    ],
+  });
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+  };
+  const url = "https://api.line.me/v2/bot/message/push";
+  const request = https.request(url, options);
+  request.write(data);
+  request.end();
+};
