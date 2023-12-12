a09d15a65fe5b2546cbe80d59f6a59f4dc2929c3
yukendev <yuken0906dev@gmail.com>
2023-12-12 22:35:46 +0900

    chore: daily test

--- src/jobs/daily.ts
+++ src/jobs/daily.ts
@@ -7,6 +7,6 @@ const myId = process.env.YUKEN_LINE_ID;
 if (myId != undefined) {
   sendLineMessage(
     myId,
-    `現在の時刻は${new Date()}、定期実行ジョブが実行されました。サーバーは正常に動作しています。`
+    `現在の時刻は${new Date()}、予定していた実行時間はシンガポール時間で13時です定期実行ジョブが実行されました。サーバーは正常に動作しています。`
   );
 }
