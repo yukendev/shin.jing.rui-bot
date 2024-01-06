ac80f9310f74506eee611d504d262c507970d883
yukendev <yuken0906dev@gmail.com>
2024-01-06 13:43:27 +0900

    feat: implement calendar utils

---
+++ src/utils/calendar.ts
@@ -0,0 +1,60 @@
+import { type calendar_v3 } from "googleapis";
+
+export const parseCalendarEvent = (
+  events: calendar_v3.Schema$Event[]
+): Pick<calendar_v3.Schema$Event, "summary" | "start" | "end">[] => {
+  return events.map((e) => {
+    return {
+      summary: e.summary,
+      start: e.start,
+      end: e.end,
+    };
+  });
+};
+
+const formatStartDate = (date: string | null | undefined) => {
+  if (!date) return "不明";
+
+  return new Date(date)
+    .toLocaleString("ja-JP", {
+      month: "2-digit",
+      day: "2-digit",
+      hour: "2-digit",
+      minute: "2-digit",
+    })
+    .replace(/月/g, "/")
+    .replace(/日/g, "")
+    .replace(/,/g, "");
+};
+
+const formatEndDate = (date: string | null | undefined) => {
+  if (!date) return "不明";
+
+  return new Date(date).toLocaleString("ja-JP", {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+};
+
+export const parseToLineMessage = (
+  events: Pick<calendar_v3.Schema$Event, "summary" | "start" | "end">[]
+) => {
+  const formatDateRange = (
+    start: string | null | undefined,
+    end: string | null | undefined
+  ) => {
+    const formattedStart = formatStartDate(start);
+    const formattedEnd = formatEndDate(end);
+    return `${formattedStart} ~ ${formattedEnd}`;
+  };
+
+  return events
+    .map(
+      (event) =>
+        `・${event.summary}: ${formatDateRange(
+          event.start?.dateTime,
+          event.end?.dateTime
+        )}`
+    )
+    .join("\n");
+};
