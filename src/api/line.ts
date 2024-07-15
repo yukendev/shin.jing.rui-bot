386b5cf3a48417b6c82ea70e72fe99e112505c32
yukendev <yuken0906dev@gmail.com>
2024-07-15 11:21:24 +0900

    feat: error handling

--- src/api/line.ts
+++ src/api/line.ts
@@ -3,6 +3,11 @@ import https from "https";
 export const sendLineMessage = (id: string, message: string) => {
   const token = process.env.LINE_ACCESS_TOKEN;
 
+  if (!token) {
+    console.error("LINE_ACCESS_TOKEN is not defined.");
+    return;
+  }
+
   const data = JSON.stringify({
     to: id,
     messages: [
@@ -21,6 +26,9 @@ export const sendLineMessage = (id: string, message: string) => {
   };
   const url = "https://api.line.me/v2/bot/message/push";
   const request = https.request(url, options);
+  request.on("error", (error) => {
+    console.error("Request error:", error);
+  });
   request.write(data);
   request.end();
 };
